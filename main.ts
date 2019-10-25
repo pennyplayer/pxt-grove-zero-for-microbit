fdf49b24123bbcb1e57dc7d74d4d67931a941c43
Penny Player <peneloperuthplayer@gmail.com>
2019-10-25 16:02:32 +1100

    unified neopixel, bitkit and custom blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,153 @@
+/**
+ * Calibration blocks
+ */
+//% weight=48 color=#FF622E icon="\uf0ad" block="Calibrate"
+//% groups="['Calibration', 'Setup']"
+namespace calibrate {
+    /**
+     * Move the micro:car forwards for 5 seconds then measure and see how straight it goes
+     * no input
+     */
+    //% weight=96
+    //% block="forward calibrate"
+    //% group="Calibration"
+    export function forward() {
+        BitKit.setMotormoduleSpeed(255, 255);
+        basic.pause(8000); //8 seconds
+        BitKit.setMotormoduleSpeed(0, 0);
+    }
+
+    /**
+    * Rotate the micro:car for 5 seconds then measure how far it got
+    * no input
+    */
+    //% weight=96
+    //% block="rotation calibrate"
+    //% group="Calibration"
+    export function rotates() {
+        BitKit.setMotormoduleSpeed(-255, 255);
+        basic.pause(10000); //5 seconds
+        BitKit.setMotormoduleSpeed(0, 0);
+    }
+}
+
+/**
+ * Grid Blocks
+ */
+//% weight=48 color=#E23405 icon="\uf009" block="Grid"
+//% groups="['Setup', 'Grid']"
+namespace grid {
+    let rcal = 1;
+    let fcal = 1;
+    let flcal = 0;
+    let frcal = 0;
+    /**
+    * Move the micro:car forwards for 5 seconds then measure and see how straight it goes
+    * no input
+    */
+    //% weight=96
+    //% block="forward one space"
+    //% group="Grid"
+    export function forward() {
+        BitKit.setMotormoduleSpeed(255 - flcal, 255 - frcal);
+        basic.pause(2000); //2 seconds???
+        BitKit.setMotormoduleSpeed(0, 0);
+    }
+    /**
+    * Turn Right
+    * no input
+    */
+    //% weight=96
+    //% block="turn right"
+    //% group="Grid"
+    export function turnright() {
+        BitKit.setMotormoduleSpeed(255, -255);
+        basic.pause(2000); //2 seconds???
+        BitKit.setMotormoduleSpeed(0, 0);
+    }
+    /**
+    * Turn Right
+    * no input
+    */
+    //% weight=96
+    //% block="turn left"
+    //% group="Grid"
+    export function turnleft() {
+        BitKit.setMotormoduleSpeed(-255, 255);
+        basic.pause(2000); //2 seconds???
+        BitKit.setMotormoduleSpeed(0, 0);
+    }
+
+    /**
+    * Red light on coral
+    * mimic the colour of the ground under the bot.
+    * eg. see red, show red, see blue, show blue
+    */
+    //% weight=96
+    //% block="on coral show red"
+    //% group="Grid"
+    export function onAUVColour(handler: () => void) {
+        let event = ColorEvent.R; //colour is red
+        const eventId = driver.subscribeToEventSource(SensorType.Liner);
+        control.onEvent(eventId, event, handler);
+        //could try changing handler to show red? Neopixel, pin0 red??
+    }
+
+    /**
+    * Calibration setup
+    * two inputs
+    */
+    //% weight=96
+    //% block="setup f:|%fcal r:|%rcal"
+    //% group="Setup"
+    export function setup(forwardinput: number, rotationinput: number) {
+        rcal = rotationinput * 1; //dark magic
+        if (forwardinput > 0) {
+            flcal = forwardinput;
+        }
+        else if (forwardinput < 0) {
+            frcal = -forwardinput;
+        }
+        else {
+            //error please enter number
+        }
+    }
+}
+/**
+ * Digger Blocks
+ */
+//% weight=48 color=#ffb612 icon="\uf0e7" block="Digger"
+//% groups="['funfun']"
+namespace digger {
+    /**
+    * Make explosion movement and sound
+    * rattle motors, make noise on speaker, show pretty picture on face
+    */
+    //% weight=96
+    //% block="EXPLODE"
+    //% group="funfun"
+    export function explode() {
+        let strip = neopixel.create(DigitalPin.P1, 4, NeoPixelMode.RGB)
+        let timer = 5
+        for (let index = 0; index <= timer; index++) {
+            led.plot(2, 2)
+            BitKit.setMotormoduleSpeed(-255, 255)
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+            basic.pause((timer - index) * 40)
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+            basic.pause((timer - index) * 40)
+            basic.clearScreen()
+            BitKit.setMotormoduleSpeed(255, -255)
+            strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+            basic.pause((timer - index) * 40)
+            strip.showColor(neopixel.colors(NeoPixelColors.White))
+            basic.pause((timer - index) * 40)
+        }
+        BitKit.setMotormoduleSpeed(0, 0);
+        music.playTone(988, 100);
+    }
+}
+
 enum SensorType {
     //% block=Sound Sensor
     Sound = 6,
@@ -76,176 +226,3 @@ enum MotionTpye {
     Auto = 1
 };
 
-/**
- * Extension blocks
- */
-//% weight=48 color=#0063A0 icon="\uf018" block="BitKit"
-//% groups="['Color Line Follower', 'Chassis']"
-namespace BitKit {
-
-    /**
-     * Set the actions and the moving speed of motormodule.
-     * @param direction the direction that want to set.
-     * @param speed the speed that want to run.
-     */
-    //% blockId=motor_set_action block="Chassis go|%direction|at|%speed speed"
-    //% weight=100
-    //% group="Chassis"
-    export function setMotormoduleAction(direction: DirectionTpye, speed: SpeedTpye) {
-        let data: Buffer = pins.createBuffer(5);
-        data[0] = 0x01;
-        if(direction == DirectionTpye.Forward){
-            data[1] = speed & 0xff;
-            data[2] = (speed >> 8) & 0xff;
-            data[3] = speed & 0xff;
-            data[4] = (speed >> 8) & 0xff;        
-        }
-        else if(direction == DirectionTpye.Backward){
-            data[1] = (-speed) & 0xff;
-            data[2] = ((-speed) >> 8) & 0xff;
-            data[3] = (-speed) & 0xff;
-            data[4] = ((-speed) >> 8) & 0xff;  
-        }
-        else if(direction == DirectionTpye.Left){
-            data[1] = 0 & 0xff;
-            data[2] = (0 >> 8) & 0xff;
-            data[3] = speed & 0xff;
-            data[4] = (speed >> 8) & 0xff;  
-        }
-        else if(direction == DirectionTpye.Right){
-            data[1] = speed & 0xff;
-            data[2] = (speed >> 8) & 0xff;
-            data[3] = 0 & 0xff;
-            data[4] = (0 >> 8) & 0xff;        
-        }
-        else if(direction == DirectionTpye.Clockwise){
-            data[1] = speed & 0xff;
-            data[2] = (speed >> 8) & 0xff;
-            data[3] = (-speed) & 0xff;
-            data[4] = ((-speed) >> 8) & 0xff;        
-        }
-        else if(direction == DirectionTpye.Anticlockwise){
-            data[1] = (-speed) & 0xff;
-            data[2] = ((-speed) >> 8) & 0xff;
-            data[3] = speed & 0xff;
-            data[4] = (speed >> 8) & 0xff;        
-        }
-        driver.i2cSendBytes(MotorTpye.Wheel, data);
-    }
-
-    /**
-     * Stop the motormodule.
-     */
-    //% blockId=motor_stop_run block="Chassis stop"
-    //% weight=99
-    //% group="Chassis"
-    export function stopMotormodule() {
-        setMotormoduleSpeed(0, 0);
-    }
-
-    /**
-     * Set the speed of motors on motormodule.
-     * @param left the left speed you want to run.
-     * @param right the right speed you want to run.
-     */
-    //% blockId=motor_set_speed_with_duty block="Chassis left motor|%left|, right motor |%right"
-    //% left.min=-255 left.max=255 left.defl=0
-    //% right.min=-255 right.max=255 right.defl=0
-    //% weight=98
-    //% group="Chassis"
-    export function setMotormoduleSpeed(left: number, right: number) {
-        let data: Buffer = pins.createBuffer(5);
-        data[0] = 0x01;
-        data[1] = left & 0xff;
-        data[2] = (left >> 8) & 0xff;
-        data[3] = right & 0xff;
-        data[4] = (right >> 8) & 0xff;
-        driver.i2cSendBytes(MotorTpye.Wheel, data);
-    }
-
-    export let linerEventValue = 0;
-    const eventIdLiner = 9000;
-    let initLiner = false;
-    let lastLiner = 0;
-
-    /**
-     * Do something when the line follower recognized the position of the line underneath.
-     * @param event type of liner to detect
-     * @param handler code to run
-     */
-    //% blockId=sensor_liner_create_event block="on Color Line Follower line position|%event"
-    //% weight=100 
-    //% group="Color Line Follower"
-    export function onLinePosition(event: LinerEvent, handler: () => void) {
-        control.onEvent(eventIdLiner, event, handler);
-        if (!initLiner) {
-            initLiner = true;
-            control.inBackground(() => {
-                while (true) {
-                    driver.i2cSendByte(SensorType.Liner, 0x02);
-                    const event = driver.i2cReceiveByte(SensorType.Liner);
-                    if (event > 2) linerEventValue = event;
-                    if (event != lastLiner) {
-                        lastLiner = event;
-                        control.raiseEvent(eventIdLiner, lastLiner);
-                    }
-                    basic.pause(50);
-                }
-            })
-        }
-    }
-
-    /**
-     * Do something when the color sensor detects a specific color.
-     * @param event type of color to detect
-     * @param handler code to run
-     */
-    //% blockId=sensor_color_create_event block="on Color Line Follower see |%event"
-    //% weight=99
-    //% group="Color Line Follower"
-    export function onColor(event: ColorEvent, handler: () => void) {
-        const eventId = driver.subscribeToEventSource(SensorType.Liner);
-        control.onEvent(eventId, event, handler);
-    }
-
-    /**
-     * See if the line follower recognized the position of the line underneath.
-     * @param event of liner device
-     */
-    //% blockId=sensor_is_liner_event_generate block="Color Line Follower see line at|%event|"
-    //% weight=98
-    //% group="Color Line Follower"
-    export function wasLinePositionTriggered(event: LinerEvent): boolean {
-        let eventValue = event;
-        if (!initLiner) onLinePosition(event, () => { });
-        if (lastLiner == eventValue) return true;
-        return false;
-    }
-
-    /**
-     * See if the color sensor detected a specific color.
-     * @param event of color device
-     */
-    //% blockId=sensor_is_color_event_generate block="Color Line Follower see color|%event|"
-    //% weight=97
-    //% group="Color Line Follower"
-    export function wasColorTriggered(event: ColorEvent): boolean {
-        let eventValue = event;
-        if (driver.addrBuffer[SensorType.Liner] == 0) onColor(event, () => { });
-        if (driver.lastStatus[SensorType.Liner] == eventValue) return true;
-        return false;
-    }
-
-    /**
-     * Get the color value from the color sensor in R:G:B.
-     */
-    //% blockId=sensor_get_color_rgb block="Color Line Follower color value"
-    //% weight=96
-    //% group="Color Line Follower"
-    export function getColor(): number {
-        let data: Buffer = pins.createBuffer(4);
-        driver.i2cSendByte(SensorType.Liner, 0x04);
-        data = driver.i2cReceiveBytes(SensorType.Liner, 4);
-        return (data[0] + data[1] * 256 + data[2] * 65536);
-    }
-}
